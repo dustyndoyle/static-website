a16401675e65ff54225110ec6ac8ac091be2bf09
Dustyn Doyle <dustyn@launchdm.com>
2016-06-14 21:36:52 -0400

    Add Skills and Circle percentages using Circles.js

--- resources/js/javascript.js
+++ resources/js/javascript.js
@@ -3,4 +3,19 @@ $(function() {
     // remove no-js class
     $('html').removeClass('no-js');
 
+    $( '.skill-circle' ).each( function() {
+
+        var circleId = $(this).attr('id');
+        var circlePercent = $(this).data( 'skillPercent' );
+
+        Circles.create({
+            id: circleId,
+            radius: 120,
+            value: circlePercent,
+            width: 20,
+            text: function(value){return value + '%';},
+            colors: ['#677077', '#f2b632'],
+        });
+    });
+
 });
